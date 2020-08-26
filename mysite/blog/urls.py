6b2a4d21156c105718330724a2c8f10dfa2cb600
Genry <genry6651@gmail.com>
2020-08-27 00:40:09 +0300

    css

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
+urlpatterns = {
     path('', views.index, name='home'),
-]
+    path('about', views.about, name='about'),
+    path('portfolio', views.portfolio, name='portfolio'),
+}
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
     return render(request, 'blog/index.html', {})
+
+
+def about(request):
+    return render(request, 'blog/about.html', {})
+
+
+def portfolio(request):
+    return render(request, 'blog/portfolio.html', {})
